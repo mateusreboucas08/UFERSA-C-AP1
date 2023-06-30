0497dd262fb5a7ca4a16d1f921017545cbfb845b
Mateus Rebouças <110422001+mateusreboucas08@users.noreply.github.com>
2023-06-30 08:40:40 -0300

    Update aula1.cpp

--- aula1.cpp
+++ aula1.cpp
@@ -16,3 +16,20 @@ int main(){
 	
 	return 0;
 }
+#include<stdio.h>
+#include<locale.h>
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	float b, h, a;
+	
+	printf("Digite o valor da base do retângulo: ");
+	scanf("%f", &b);
+	printf("Digite o valor da altura do retângulo: ");
+	scanf("%f", &h);
+	
+	a = b * h;
+	printf("A área do retângulo é igual a %f", a);
+	
+	return 0;
+}
