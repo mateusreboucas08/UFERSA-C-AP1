06f0b338332b2def467357bbe8f8bc8bbef25798
Mateus Rebouças <110422001+mateusreboucas08@users.noreply.github.com>
2023-07-14 07:36:18 -0300

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -3,15 +3,23 @@
 int main() {
   int x,y,z;
   printf("Digite o valor do primeiro lado: ");
-  scanf("%f", &x);
+  scanf("%d", &x);
   printf("Digite o valor do segundo lado: ");
-  scanf("%f", &y);
+  scanf("%d", &y);
   printf("Digite o valor do terceiro lado: ");
-  scanf("%f", &z);
-  if (x<y+z) && (y<x+z) && (z<x+y){
-    printf("É um triângulo");
+  scanf("%d", &z);
+  if((x<y+z) && (y<x+z) && (z<x+y)){
+    printf("É um triângulo\n");
   }else{
-    printf("Não é um triângulo");
+    printf("Não é um triângulo\n");
+  }
+  if ((x = y) && (x = z) && (y != z) ){
+  	printf("É um triângulo isóceles");
+  }else if ((x != y) && (x != z) && (y != z)){
+  	printf("É um triângulo escaleno");
+  }else{
+  	printf("É um triângulo equilátero");
   }
   return 0;
 }
+
