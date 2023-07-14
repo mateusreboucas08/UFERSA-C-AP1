8ffef80829b14d13bb314c2f812451dd07b55505
Mateus Rebouças <110422001+mateusreboucas08@users.noreply.github.com>
2023-07-14 07:22:45 -0300

    Update lista.cpp

--- lista.cpp
+++ lista.cpp
@@ -2,8 +2,16 @@
 
 int main() {
   int x,y,z;
+  printf("Digite o valor do primeiro lado: ");
+  scanf("%f", &x);
+  printf("Digite o valor do segundo lado: ");
+  scanf("%f", &y);
+  printf("Digite o valor do terceiro lado: ");
+  scanf("%f", &z);
   if (x<y+z) && (y<x+z) && (z<x+y){
-    
+    printf("É um triângulo");
+  }else{
+    printf("Não é um triângulo");
   }
   return 0;
 }
