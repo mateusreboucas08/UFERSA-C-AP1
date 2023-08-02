d7205f3175431015f8ef63f893cff8dacc62cb24
Mateus Rebouças <110422001+mateusreboucas08@users.noreply.github.com>
2023-08-02 08:33:38 -0300

    Update AULA0208.cpp

--- AULA0208.cpp
+++ AULA0208.cpp
@@ -10,6 +10,59 @@ int main(){
 	}
   return 0;
 }
+#include <stdio.h>
+#include <locale>
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int n,qtdMultiplos, i;
+	printf("Digite um número: ");
+	scanf("%d", &n);
+	printf("Digite a quantidade de múltiplos de %d a serem impressos: ", i);
+	scanf("%d", &qtdMultiplos);
+	i=1;
+	while(i <= qtdMultiplos){
+		printf("%d\n",n*i);
+		i= i + 1;
+	}
+	return 0;
+}
+
+#include <stdio.h>
+#include <locale>
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int sup, inf, i;
+	printf("Digite o valor do limite inferior: ");
+	scanf("%d",&inf);
+	printf("Digite o valor do limite superior: ");
+	scanf("%d",&sup);
+	printf("A sequência que vai de %d a %d é: ", inf, sup);
+	i = inf;
+	while(i <= sup){
+		printf("%d ", i);
+		i++;
+	}
+	return 0;
+}
+#include <stdio.h>
+#include <locale>
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	int num, i;
+	printf("Digite um número: ");
+	scanf("%d", &num);
+	printf("Os divisores de %d são: ", num);
+	i = 1;
+	while (i<=num){
+		if (num % i == 0){
+			printf("%d ", i);
+			if (i < num) {
+				printf(", ");
+			}
+		}
+		i++;
+	}
+}
 
 ////// Laço For //////
 
