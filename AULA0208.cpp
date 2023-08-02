511d8886759926b78df81d4a35788edfebe67a3c
Mateus Rebouças <110422001+mateusreboucas08@users.noreply.github.com>
2023-08-02 07:24:45 -0300

    Create AULA0208.cpp

---
+++ AULA0208.cpp
@@ -0,0 +1,16 @@
+////// Laço While //////
+
+#include <stdio.h>
+#include <locale>
+int main(){
+	int i = 0;
+	while(i != 10001){
+		printf("%d\n",i);
+		i=i+1;
+	}
+  return 0;
+}
+
+////// Laço For //////
+
+
